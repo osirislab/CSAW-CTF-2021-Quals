48248c5a3108154d035138e3f18ad3db370f0a37
victini-lover <drs9930@nyu.edu>
2021-06-24 17:10:33 -0400

    Updated generator.py to properly work with docker container

--- crypto/RSA-Pop-Quiz/generator.py
+++ crypto/RSA-Pop-Quiz/generator.py
@@ -1,11 +1,11 @@
-from Crypto.Util.number import getPrime, bytes_to_long
+from Crypto.Util.number import getPrime, bytes_to_long, inverse
 import random, math
 from sympy import isprime
 
 with open("flag.txt",'r') as f:
 	flag = f.read()
 
-m1 = "Weiner weiner chicken dinner" # For wiener's attack
+m1 = "Wiener wiener chicken dinner" # For wiener's attack
 m2 = "Man, these primes sure are sexy" # For sexy primes
 m3 = "Wow, looks like this oracle is worthless" # For LSB oracle
 m4 = "I'll be careful next time to not leak the key" # For partial key
@@ -21,7 +21,8 @@ def wiener_attack():
 	while True:
 		try:
 			d = random.randint(2,pow(N,1/4)//3)
-			e = pow(d,-1,phi)
+			print(d)
+			e = inverse(d,phi)
 			break
 		except:
 			continue
@@ -39,7 +40,7 @@ def sexy_primes():
 		if isprime(q):
 			N = p*q
 			phi = (p-1)*(q-1)
-			d = pow(e,-1,phi)
+			d = inverse(e,phi)
 			print("N =",N)
 			print("e =",e)
 			print("c =",pow(m,e,N))
@@ -53,12 +54,13 @@ def lsb_oracle():
 	N = p*q
 	phi = (p-1)*(q-1)
 	e = 65537
-	d = pow(e,-1,phi)
+	d = inverse(e,phi)
 	print("N =",N)
 	print("e =",e)
 	print("c =",pow(m,e,N))
 	for i in range(int(math.log(N,2))+1):
-		given = int(input("\nWhat would you like to decrypt?: "))
+		print("\nWhat would you like to decrypt?")
+		given = int(input(""))
 		decrypt = pow(given,d,N)
 		print("\nThe oracle responds with:",bin(decrypt)[-1])
 
@@ -72,7 +74,7 @@ def partial_key():
 			N = p*q
 			phi = (p-1)*(q-1)
 			e = 17
-			d = pow(e,-1,phi)
+			d = inverse(e,phi)
 			break
 		except:
 			continue
@@ -91,7 +93,8 @@ def main():
 	print("Part 1 --> This is one of the most common RSA attacks in CTFs!\n")
 	wiener_attack()
 	while True:
-		answer = input("\nWhat is the plaintext?: ")
+		print("\nWhat is the plaintext?")
+		answer = input("")
 		if answer == m1:
 			print("Success!")
 			break
@@ -101,7 +104,8 @@ def main():
 	print("Part 2 --> Sexy numbers were used to make the modulus!\n")
 	sexy_primes()
 	while True:
-		answer = input("\nWhat is the plaintext?: ")
+		print("\nWhat is the plaintext?")
+		answer = input("")
 		if answer == m2:
 			print("Success!")
 			break
@@ -111,7 +115,8 @@ def main():
 	print("Part 3 --> Looks like there is a oracle which is telling the LSB of the plaintext. That will not help you, right?\n")
 	lsb_oracle()
 	while True:
-		answer = input("\nWhat is the plaintext?: ")
+		print("\nWhat is the plaintext?")
+		answer = input("")
 		if answer == m3:
 			print("Success!")
 			break
@@ -121,7 +126,8 @@ def main():
 	print("Part 4 --> Oops, looks like I leaked part of the private key. Hope that doesn't come back to bite me\n")
 	partial_key()
 	while True:
-		answer = input("\nWhat is the plaintext?: ")
+		print("\nWhat is the plaintext?")
+		answer = input("")
 		if answer == m4:
 			print("Success!")
 			break
@@ -131,7 +137,8 @@ def main():
 	"""print("Part 5\n")
 	rsa_crt()
 	while True:
-		answer = input("\nWhat is the plaintext?: ")
+		print("\nWhat is the plaintext?")
+		answer = input("")
 		if answer == m5:
 			print("Success!")
 			break
