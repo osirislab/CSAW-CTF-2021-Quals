0ecf1f3f9cf06a2e69b207cbd631230da07c5d1a
victini-lover <drs9930@nyu.edu>
2021-07-04 15:10:17 -0400

    Finished implementation of QKD challenge

--- misc/Save-the-Tristate/save_tristate.py
+++ misc/Save-the-Tristate/save_tristate.py
@@ -1,6 +1,5 @@
 import random
 
-
 #Create randomized base stream (true_stream)
 def base_stream():
 	rand_stream = ''
@@ -10,12 +9,11 @@ def base_stream():
 			rand_stream += chr(120)
 		else:
 			rand_stream += chr(43)
-	print(rand_stream)
 	return rand_stream
 
 
 #Checking challenger's stream of bases
-def stream_check():
+def stream_check(true_stream):
 	errs = 0
 	#Enter the number of bases you'd like to check
 	print("How many bases would you like to check? ")
@@ -33,23 +31,42 @@ def stream_check():
 
 	if (errs == 0) and (str_len == len(true_stream)):
 		#give key
-		with open('key.txt','r') as f:
-			key = f.read()
-		res = ''.join(format(ord(i), '08b') for i in key)
-		print("The original string is : " + key)
-		print("The string after binary conversion : " + res)
-		#Check if arrows are correct
-		def get_arrows():
-			if basis == '+':
-			    if bit == 1:
-			        print('0.0 + 1.0i')
-			    else: # bit == 0
-			        print('1.0 + 0.0i')
-			else: # basis == 'x'
-			    if bit == 1:
-			        print('-0.707 + 0.707i')
-			    else: # bit == 0
-			        print('0.707 + 0.707i')
-	return errs
+		return True
+	return False
 
+#Check if arrows are correct
+def get_arrows(basis, bit):
+	if basis == '+':
+		if bit == '1':
+			print('0.0 + 1.0i')
+		else: # bit == '0'
+			print('1.0 + 0.0i')
+	else: # basis == 'x'
+		if bit == '1':
+			print('-0.707 + 0.707i')
+		else: # bit == '0'
+			print('0.707 + 0.707i')
 
+def main():
+	our_basis = base_stream()
+	with open('key.txt','r') as f:
+		key = f.read()
+	with open('flag.txt','r') as f:
+		flag = f.read()
+	while True:
+		if stream_check(our_basis):
+			res = ''.join(format(ord(i), '08b') for i in key)
+			for i in range(len(res)):
+				get_arrows(our_basis[i], res[i])
+			break
+		else:
+			continue
+	print('What is the key?: ')
+	key_input = input('')
+	if key_input==key:
+		print(flag)
+	else:
+		print('You failed Perry the Platypus! I, Heinz Doofenshmirtz, have won!!!')
+
+if __name__ == "__main__":
+	main()
